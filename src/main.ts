c09c490c7f3bb2781ab2abf4b0adb84dada09abc
DotNet14 <naizuddin12@gmail.com>
2025-12-14 00:55:25 +0530

    Certificate 10

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { Doc } from './app/doc/doc';
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(Doc, appConfig)
   .catch((err) => console.error(err));
